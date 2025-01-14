7d62f5de9fc9f21a9c3313dda4a9d55df941976d
Thomas Junk <thomas.junk@intevation.de>
2025-01-14 13:11:47 +0100

    Adding Tests for subselect

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/formula/hhhNNN/Person.java
@@ -0,0 +1,49 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.orm.test.mapping.formula.hhhNNN;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.Set;
+
+
+@Entity
+class Person {
+
+	@Id
+	private Long id;
+
+	@OneToMany(mappedBy = "author")
+	private Set<Book> booksWritten;
+
+	@OneToMany(mappedBy = "translator")
+	private Set<Book> booksTranslated;
+
+	public Set<Book> getBooksWritten() {
+		return this.booksWritten;
+	}
+
+	public void setBooksWritten(Set<Book> booksWritten){
+		this.booksWritten = booksWritten;
+	}
+
+	public Set<Book> getBooksTranslated() {
+		return this.booksTranslated;
+	}
+
+	public void setBooksTranslated(Set<Book> booksTranslated){
+		this.booksTranslated = booksTranslated;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
